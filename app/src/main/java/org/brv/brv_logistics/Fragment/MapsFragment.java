9a7a09d6526509eadc5573c6d6e6e648c65e1e92
brvltd <bravie068@gmail.com>
2020-10-24 13:38:02 +0300

    today's updates

--- app/src/main/java/org/brv/brv_logistics/Fragment/MapsFragment.java
+++ app/src/main/java/org/brv/brv_logistics/Fragment/MapsFragment.java
@@ -12,6 +12,10 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,11 +26,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.brv.brv_logistics.R;
 
 
+
 import static org.brv.brv_logistics.Constants.Constants.DEFAULT_ZOOM;
 
 public class MapsFragment extends Fragment {
     GoogleMap map;
     private View mapView;
+
+
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
         @SuppressLint("MissingPermission")
         @Override
@@ -59,8 +66,33 @@ public class MapsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        try {
+            mapView=inflater.inflate(R.layout.fragment_maps,container,false);
+//            PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+//                    getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+//
+//            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//                @Override
+//                public void onPlaceSelected(Place place) {
+//                    map.clear();
+//                    map.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()));
+//                    map.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+//                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 12.0f));
+//                }
+//
+//                @Override
+//                public void onError(Status status) {
+//
+//                }
+//            });
+        }catch (Exception ex){
+            Toast.makeText(getActivity(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+
+        return mapView;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -70,5 +102,8 @@ public class MapsFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+
     }
+
+
 }
--- app/src/main/java/org/brv/brv_logistics/Fragment/RegisterFragment.java
+++ app/src/main/java/org/brv/brv_logistics/Fragment/RegisterFragment.java
@@ -4,26 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.common.api.Status;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import org.brv.brv_logistics.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class RegisterFragment extends Fragment implements PlaceSelectionListener {
-    @BindView(R.id.from) SearchView from;
-    @BindView(R.id.to) SearchView to;
+public class RegisterFragment extends Fragment {
 
     View v;
 
@@ -33,42 +24,13 @@ public class RegisterFragment extends Fragment implements PlaceSelectionListener
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       v=inflater.inflate(R.layout.register_cargo,container,false);
-        ButterKnife.bind(this,v);
-        from.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                Toast.makeText(getActivity(), "from", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-        to.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                Toast.makeText(getActivity(), "To", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
+       try {
+           v=inflater.inflate(R.layout.register_cargo,container,false);
+       }catch (Exception ex){
+           Toast.makeText(getActivity(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+       }
         return v;
     }
 
-    @Override
-    public void onPlaceSelected(@NonNull Place place) {
 
-    }
-
-    @Override
-    public void onError(@NonNull Status status) {
-
-    }
 }
